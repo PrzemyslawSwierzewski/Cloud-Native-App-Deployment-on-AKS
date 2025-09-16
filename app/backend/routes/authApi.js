203e46e540f31af3f2a70fe7b69bc57addb51ed1
PrzemyslawSwierzewski <110669323+PrzemyslawSwierzewski@users.noreply.github.com>
2025-09-16 20:00:49 +0200

    Update authApi.js

--- app/backend/routes/authApi.js
+++ app/backend/routes/authApi.js
@@ -13,10 +13,10 @@ const { loginDemo } = require("../controllers/authControllerDemo");
 
 // use {login } from authController , uncomment line below
 
-// router.route("/login").post(catchErrors(login));
+router.route("/login").post(catchErrors(login));
 
 // for development & production don't use this line router.route("/login").post(catchErrors(loginDemo)); (you should remove it) , this is just demo login contoller
-router.route("/login").post(catchErrors(loginDemo));
+//router.route("/login").post(catchErrors(loginDemo));
 router.route("/logout").post(isValidToken, catchErrors(logout));
 
 module.exports = router;
