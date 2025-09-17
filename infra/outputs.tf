63a9696aea1c466adb7d2fabb6512b3a69e5b0fc
PrzemyslawSwierzewski <110669323+PrzemyslawSwierzewski@users.noreply.github.com>
2025-09-17 20:39:47 +0200

    Workflow check pt.2

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -9,3 +9,27 @@ output "prod_acr" {
 output "prod_aks_cluster_name" {
   value = module.prod_aks.aks_cluster_name
 }
+
+output "staging_resource_group" {
+  value = local.environments.stage.rg_name
+}
+
+output "staging_acr" {
+  value = module.stage_acr.acr_name
+}
+
+output "staging_aks_cluster_name" {
+  value = module.stage_aks.aks_cluster_name
+}
+
+output "dev_resource_group" {
+  value = local.environments.dev.rg_name
+}
+
+output "dev_acr" {
+  value = module.dev_acr.acr_name
+}
+
+output "dev_aks_cluster_name" {
+  value = module.dev_aks.aks_cluster_name
+}
